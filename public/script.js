93b41ff28a3dda810ae69aecbc828f026255f033
Abraham <abrahammartinez2000@gmail.com>
2024-12-05 00:07:37 +0100

    Commit v3

--- public/script.js
+++ public/script.js
@@ -1,14 +1,4 @@
 
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-    alert(`Bienvenido ${username}`);
-});
-
-
-
-
 // Rellenar credenciales de usuario para pruebas
 function rellenarCredenciales(usuario, contrasena) {
     document.getElementById('usuario').value = usuario;
